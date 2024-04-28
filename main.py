e638be57f5d48d4a56c666831b2106b5e439a408
Pavitra Agarwal <pavitra@Pavitras-MacBook-Air.local>
2024-04-29 00:36:43 +0530

    changing the header length

--- main.py
+++ main.py
@@ -193,7 +193,7 @@ while True :
   cout +=1 
   # print(cout)
 
-block_header = hash256(block.serialize())[::-1].hex()
+block_header = blockid
 Coinbase_txn_serialize = ctx.serialize().hex()
 Coinbase_txn_id = ctx.id() 
 # print(block_header)
